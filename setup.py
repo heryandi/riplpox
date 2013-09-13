a031ce812151efbf67d32439d0428a931a984c56
Heryandi <heryandi@gmail.com>
2013-09-13 15:42:01 +0800

    Add classifier

--- setup.py
+++ setup.py
@@ -11,18 +11,20 @@ setup(
     author_email='brandonh@stanford.edu',
     packages=find_packages(exclude='test'),
     long_description=open('README').read(),
-      classifiers=[
-          "License :: OSI Approved :: GNU General Public License (GPL)",
-          "Programming Language :: Python",
-          "Development Status :: 4 - Beta",
-          "Intended Audience :: Developers",
-          "Topic :: Internet",
-      ],
-      keywords='networking protocol Internet OpenFlow data center datacenter',
-      license='GPL',
-      install_requires=[
+    classifiers=[
+        "OpenFlow :: Controller",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Programming Language :: Python",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Topic :: Internet",
+    ],
+    keywords='networking protocol Internet OpenFlow data center datacenter',
+    license='GPL',
+    install_requires=[
         'ripl',
         'pox',
         'setuptools',
         'networkx'
-      ])
+    ]
+)
